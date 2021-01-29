17283a65854a40bdb6a7a9cca398293dafb4ddd1
mm <meng.wang@lmfun.cn>
2021-01-29 14:01:41 +0800

    init

---
+++ src/main/java/com/xd/smartconstruction/SmartConstructionApplication.java
@@ -0,0 +1,13 @@
+package com.xd.smartconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartConstructionApplication.class, args);
+	}
+
+}
