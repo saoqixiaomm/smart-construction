6dbbaea35615f1de351800d96bf1149df17d2023
mm <meng.wang@lmfun.cn>
2021-02-01 15:19:58 +0800

    新闻模板

---
+++ src/main/java/com/xd/smartconstruction/controller/news/NewsController.java
@@ -0,0 +1,17 @@
+package com.xd.smartconstruction.controller.news;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mm
+ * @Date 2021-02-01 15:19
+ */
+@Api("新闻模块")
+@RestController
+@RequestMapping("/account")
+@Slf4j
+public class NewsController {
+}
